2c2683c87ccd7abef3031cd7285affe94221573e
Matthew Weier O'Phinney <matthew@weierophinney.net>
2008-12-03 15:44:04 -0500

    Created ExpandoPane for nav pane; contains AccordionContainer for menu

--- public/js-src/spindle/main.js
+++ public/js-src/spindle/main.js
@@ -3,6 +3,8 @@ dojo.provide("spindle.main");
 (function() {
     dojo.require("dijit.layout.BorderContainer");
     dojo.require("dijit.layout.ContentPane");
+    dojo.require("dojox.layout.ExpandoPane");
+    dojo.require("dijit.layout.AccordionContainer");
     dojo.require("dojo.parser");
 
     // Derive base URL
