fbd80f1136e61b13c580593fdb6771a2e9ef108b
Matthew Weier O'Phinney <matthew@weierophinney.net>
2008-12-04 00:26:59 -0500

    Moved NavMenu to JS layer; modified links to call JS functionality

--- public/js-src/spindle/main.js
+++ public/js-src/spindle/main.js
@@ -1,11 +1,18 @@
 dojo.provide("spindle.main");
 
 (function() {
+    dojo.require("spindle.NavMenu");
     dojo.require("dijit.layout.BorderContainer");
     dojo.require("dijit.layout.ContentPane");
-    dojo.require("dojox.layout.ExpandoPane");
-    dojo.require("dijit.layout.AccordionContainer");
     dojo.require("dojo.parser");
+
+    dojo.addOnLoad(function() {
+        spindle.navMenu = new spindle.NavMenu({
+            baseUrl: spindle.baseUrl,
+        });
+        dijit.byId("layout").addChild(spindle.navMenu);
+        spindle.navMenu.startup();
+    });
 
     // Derive base URL
     var path      = window.location.pathname;
